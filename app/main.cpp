e12564199247ed418ef04305597d78f987ba90f8
Manuel Meraz <meraz.manuel@gmail.com>
2020-09-28 14:26:55 -0400

    enable modules

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,60 @@
-#include <cppcoro/task.hpp>
 #include <cppcoro/sync_wait.hpp>
+#include <cppcoro/task.hpp>
+#include <cppcoro/static_thread_pool.hpp>
 #include <cppcoro/when_all.hpp>
-#include <cstdio>
 
-using namespace cppcoro;
-task<> work(std::size_t num_work) {
-  while (num_work) {
-    puts("Doing work!");
-    --num_work;
-  }
-  co_return;
+import std;
+
+using namespace std::chrono_literals;
+
+cppcoro::task<std::string> getFirst() {
+  std::this_thread::sleep_for(1s);
+  co_return "First";
+}
+
+cppcoro::task<std::string> getSecond() {
+  std::this_thread::sleep_for(1s);
+  co_return "Second";
+}
+
+cppcoro::task<std::string> getThird() {
+  std::this_thread::sleep_for(1s);
+  co_return "Third";
+}
+
+template <typename Func>
+cppcoro::task<std::string> runOnThreadPool(cppcoro::static_thread_pool& tp, Func func) {
+  co_await tp.schedule();
+  auto res = co_await func();
+  co_return res;
+}
+
+cppcoro::task<> runAll(cppcoro::static_thread_pool& tp) {
+
+  auto[fir, sec, thi] = co_await cppcoro::when_all(    // (3)
+    runOnThreadPool(tp, getFirst),
+    runOnThreadPool(tp, getSecond),
+    runOnThreadPool(tp, getThird));
+
+  std::cout << fir << " " << sec << " " << thi << std::endl;
+
 }
 
 int main() {
-  when_all([]() -> task<> {
-    co_await work(2);
-  }());
+
+  std::cout << std::endl;
+
+  auto start = std::chrono::steady_clock::now();
+
+  cppcoro::static_thread_pool tp;                         // (1)
+  cppcoro::sync_wait(runAll(tp));                         // (2)
+
+  std::cout << std::endl;
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end - start;    // (4)
+  std::cout << "Execution time " << elapsed.count() << " seconds." << std::endl;
+
+  std::cout << std::endl;
+
 }
