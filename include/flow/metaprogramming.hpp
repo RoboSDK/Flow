a4f1edf4dc027d2a72a299becba38c6932fbf827
Manuel Meraz <meraz.manuel@gmail.com>
2020-10-02 19:48:48 -0400

    add tests for metaprogramming, still need to add more

--- include/flow/metaprogramming.hpp
+++ include/flow/metaprogramming.hpp
@@ -5,134 +5,174 @@
 #include <variant>
 #include <string_view>
 #include <wyhash/wyhash.h>
+#include <concepts>
 
 namespace flow::metaprogramming {
 
-/**
- * Various hasing functions.
- * source: wyhash
+/*
+ * A metaprogramming size_tc to pass in integral arguments as arguments at compile time
  */
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wold-style-cast"
-//#pragma clang diagnostic ignored "-Wshorten-64-to-32"
-//#pragma clang diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-static consteval uint64_t consteval_wyrotr(uint64_t v, unsigned k) { return (v >> k) | (v << (64 - k)); }
+template <std::size_t N>
+struct size_tc {
+  std::size_t data = N;
+};
 
-static consteval uint64_t consteval_wymum(uint64_t A, uint64_t B) {
-#ifdef WYHASH32
-  uint64_t hh = (A >> 32) * (B >> 32), hl = (A >> 32) * (unsigned)B, lh = (unsigned)A * (B >> 32), ll = (uint64_t)(unsigned)A * (unsigned)B;
-  return consteval_wyrotr(hl, 32) ^ consteval_wyrotr(lh, 32) ^ hh ^ ll;
-#else
-#ifdef __SIZEOF_INT128__
-  __uint128_t r = A;
-  r *= B;
-  return static_cast<uint64_t>(r >> 64U ^ r);
-#elif defined(_MSC_VER) && defined(_M_X64)
-  A = _umul128(A, B, &B);
-  return A ^ B;
-#else
-  uint64_t ha = A >> 32, hb = B >> 32, la = (uint32_t)A, lb = (uint32_t)B, hi, lo;
-  uint64_t rh = ha * hb, rm0 = ha * lb, rm1 = hb * la, rl = la * lb, t = rl + (rm0 << 32), c = t < rl;
-  lo = t + (rm1 << 32);
-  c += lo < t;
-  hi = rh + (rm0 >> 32) + (rm1 >> 32) + c;
-  return hi ^ lo;
-#endif
-#endif
-}
+/**
+ * Basic container type that can be used to get a type out of a tuple with a single type
+ *
+ * May also be used to store a type and use the value later
+ * @tparam t Any type
+ */
+template <typename t>
+struct type_container {
+  using type = t;
+  constexpr type_container() = default;
+  constexpr type_container(type_container<t>&&) = default;
+  constexpr type_container(type_container<t> const&) = default;
+  constexpr type_container<t>& operator=(type_container<t> &&) = default;
+  constexpr type_container<t>& operator=(type_container<t> const&) = default;
+  constexpr type_container(std::tuple<t> /*unused*/) {}
+};
 
-template<typename T>
-static consteval uint64_t consteval_wyr8(const T* p) {
-  uint64_t v = 0;
-  v = ((uint64_t)p[0] << 56U) | ((uint64_t)p[1] << 48U) | ((uint64_t)p[2] << 40U) | ((uint64_t)p[3] << 32U) | (p[4] << 24U) | (p[5] << 16U) | (p[6] << 8U) | p[7];
-  return v;
+/**
+ * Get the last of the list
+ * @tparam The list of items
+ * @return the size of the list
+ */
+template <typename... items_t>
+constexpr std::size_t size([[maybe_unused]] std::tuple<items_t...> list =  std::tuple<items_t...>{}) {
+  return sizeof...(items_t);
 }
-
-template<typename T>
-static consteval uint64_t consteval_wyr4(const T* p) {
-  uint32_t v = 0;
-  v = (p[0] << 24U) | (p[1] << 16U) | (p[2] << 8U) | p[3];
-  return v;
-}
-
-template<typename T>
-static consteval uint64_t consteval_wyr3(const T* p, unsigned k) {
-  return (((uint64_t)p[0]) << 16U) | (((uint64_t)p[k >> 1U]) << 8U) | p[k - 1];
-}
-
-template<typename T>
-static consteval uint64_t consteval_wyhash(const T* key, uint64_t len, uint64_t seed) {
-  static_assert(sizeof(T) == 1, "T must be a char or uint8_t kind type");
-#if defined(__GNUC__) || defined(__INTEL_COMPILER)
-  if (__builtin_expect(!len, 0)) return 0;
-#else
-  if (!len) return 0;
-#endif
-  const T* p = key;
-  if (len < 4)
-    return consteval_wymum(consteval_wymum(consteval_wyr3(p, len) ^ seed ^ _wyp0, seed ^ _wyp1), len ^ _wyp4);
-  else if (len <= 8)
-    return consteval_wymum(
-      consteval_wymum(consteval_wyr4(p) ^ seed ^ _wyp0, consteval_wyr4(p + len - 4) ^ seed ^ _wyp1),
-      len ^ _wyp4);
-  else if (len <= 16)
-    return consteval_wymum(
-      consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + len - 8) ^ seed ^ _wyp1),
-      len ^ _wyp4);
-  else if (len <= 24)
-    return consteval_wymum(
-      consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + len - 8) ^ seed ^ _wyp2, seed ^ _wyp3), len ^ _wyp4);
-  else if (len <= 32)
-    return consteval_wymum(
-      consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + 16) ^ seed ^ _wyp2, consteval_wyr8(p + len - 8) ^ seed ^ _wyp3),
-      len ^ _wyp4);
-  uint64_t see1 = seed, i = len;
-  if (i >= 256)
-    for (; i >= 256; i -= 256, p += 256) {
-      seed = consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + 16) ^ seed ^ _wyp2, consteval_wyr8(p + 24) ^ seed ^ _wyp3);
-      see1 = consteval_wymum(consteval_wyr8(p + 32) ^ see1 ^ _wyp1, consteval_wyr8(p + 40) ^ see1 ^ _wyp2) ^ consteval_wymum(consteval_wyr8(p + 48) ^ see1 ^ _wyp3, consteval_wyr8(p + 56) ^ see1 ^ _wyp0);
-      seed = consteval_wymum(consteval_wyr8(p + 64) ^ seed ^ _wyp0, consteval_wyr8(p + 72) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + 80) ^ seed ^ _wyp2, consteval_wyr8(p + 88) ^ seed ^ _wyp3);
-      see1 = consteval_wymum(consteval_wyr8(p + 96) ^ see1 ^ _wyp1, consteval_wyr8(p + 104) ^ see1 ^ _wyp2) ^ consteval_wymum(consteval_wyr8(p + 112) ^ see1 ^ _wyp3, consteval_wyr8(p + 120) ^ see1 ^ _wyp0);
-      seed = consteval_wymum(consteval_wyr8(p + 128) ^ seed ^ _wyp0, consteval_wyr8(p + 136) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + 144) ^ seed ^ _wyp2, consteval_wyr8(p + 152) ^ seed ^ _wyp3);
-      see1 = consteval_wymum(consteval_wyr8(p + 160) ^ see1 ^ _wyp1, consteval_wyr8(p + 168) ^ see1 ^ _wyp2) ^ consteval_wymum(consteval_wyr8(p + 176) ^ see1 ^ _wyp3, consteval_wyr8(p + 184) ^ see1 ^ _wyp0);
-      seed = consteval_wymum(consteval_wyr8(p + 192) ^ seed ^ _wyp0, consteval_wyr8(p + 200) ^ seed ^ _wyp1) ^ consteval_wymum(consteval_wyr8(p + 208) ^ seed ^ _wyp2, consteval_wyr8(p + 216) ^ seed ^ _wyp3);
-      see1 = consteval_wymum(consteval_wyr8(p + 224) ^ see1 ^ _wyp1, consteval_wyr8(p + 232) ^ see1 ^ _wyp2) ^ consteval_wymum(consteval_wyr8(p + 240) ^ see1 ^ _wyp3, consteval_wyr8(p + 248) ^ see1 ^ _wyp0);
-    }
-  for (; i >= 32; i -= 32, p += 32) {
-    seed = consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1);
-    see1 = consteval_wymum(consteval_wyr8(p + 16) ^ see1 ^ _wyp2, consteval_wyr8(p + 24) ^ see1 ^ _wyp3);
-  }
-  if (!i) {
-  } else if (i < 4)
-    seed = consteval_wymum(consteval_wyr3(p, i) ^ seed ^ _wyp0, seed ^ _wyp1);
-  else if (i <= 8)
-    seed = consteval_wymum(consteval_wyr4(p) ^ seed ^ _wyp0, consteval_wyr4(p + i - 4) ^ seed ^ _wyp1);
-  else if (i <= 16)
-    seed = consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + i - 8) ^ seed ^ _wyp1);
-  else if (i <= 24) {
-    seed = consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1);
-    see1 = consteval_wymum(consteval_wyr8(p + i - 8) ^ see1 ^ _wyp2, see1 ^ _wyp3);
-  } else {
-    seed = consteval_wymum(consteval_wyr8(p) ^ seed ^ _wyp0, consteval_wyr8(p + 8) ^ seed ^ _wyp1);
-    see1 = consteval_wymum(consteval_wyr8(p + 16) ^ see1 ^ _wyp2, consteval_wyr8(p + i - 8) ^ see1 ^ _wyp3);
-  }
-  return consteval_wymum(seed ^ see1, len ^ _wyp4);
-}
-//#pragma clang diagnostic pop
-#pragma GCC diagnostic pop
 
 /**
  * Returns whether the list passed in is empty
  * @tparam A list of types
  * @return Whether the list is empty
  */
-template <class... List>
-constexpr bool empty()
+template <class... items_t>
+constexpr bool empty([[maybe_unused]] std::tuple<items_t...> list =  std::tuple<items_t...>{})
 {
-  return sizeof...(List) == 0;
+  return sizeof...(items_t) == 0;
+}
+
+
+/**
+ * pops the first item from the list
+ * @tparam removed_t The removed type
+ * @tparam the_rest_t The rest...
+ * @return A tuple with the rest of the types
+ */
+template <typename removed_t, typename... the_rest_t>
+constexpr auto pop_front([[maybe_unused]] std::tuple<removed_t, the_rest_t...> list =  std::tuple<removed_t, the_rest_t...>{}) {
+  return std::tuple<the_rest_t...>{};
+}
+
+template <typename removed_t, typename... the_rest_t, std::size_t N>
+constexpr auto pop_front([[maybe_unused]] size_tc<N>) {
+  if constexpr (N == 0) {
+    return std::tuple<removed_t, the_rest_t...>{};
+  } else {
+    static_assert(not empty<the_rest_t...>(), "metaprogramming::pop_front: N must not be greater than the list");
+    return pop_front<the_rest_t...>(size_tc<N - 1>{});
+  }
+}
+
+/**
+ * Return the next item in the list
+ * @tparam next_t The next type
+ * @tparam the_rest_t The rest...
+ * @return A tuple with the next item
+ */
+template <typename next_t, typename... the_rest_t>
+constexpr auto next(std::tuple<next_t> next_item =  std::tuple<next_t>{}) {
+  return next_item;
+}
+
+/**
+ * Determines whether the items in the list are the same
+ * @tparam items_t Any items
+ * @param A tuple of types may be passed in to get items
+ * @return true if they're all the same type
+ */
+template <typename... items_t>
+constexpr bool is_same(std::tuple<items_t...> list =  std::tuple<items_t...>{}) {
+  if constexpr (empty(list) or size(list) == 1) {
+    return true;
+  } else {
+    constexpr auto first = type_container(next(list));
+    using first_type = typename decltype(first)::type;
+
+    constexpr auto the_rest = pop_front(list);
+    constexpr auto second = type_container(next(the_rest));
+    using second_type = typename decltype(second)::type;
+
+    constexpr bool same = std::is_same_v<first_type, second_type>;
+    return same and is_same(pop_front(the_rest));
+  }
+}
+
+//
+//template <typename t1, typename t2>
+//constexpr auto join() {
+// return type_list<t1, t2>{};
+//}
+//
+//template <typename t1>
+//constexpr auto join<t1, empty_t>() {
+//  return type_list<t1>{};
+//}
+//
+//template <typename current, typename... the_rest_t>
+//constexpr auto pop_back() {
+//  constexpr std::size_t list_size = size<the_rest_t...>();
+//  if constexpr (list_size == 1) {
+//    return type_container<current>{};
+//  } else if constexpr (list_size > 1) {
+//    auto popped_back = pop_back<the_rest_t...>();
+//  } else if constexpr (list_size == 0) {
+//    return type_container<empty_t>{};
+//  }
+//}
+
+
+/**
+ * Pops the front of the list and continues the for loop
+ * @tparam The message to be popped
+ * @tparam The rest of the items
+ * @param The callback the for loop is executing
+ */
+template<typename completed_t, typename... message_ts>
+[[maybe_unused]] void pop_and_continue(auto&& callback);
+
+/**
+ * Iterates through each type and applies it to a callback that takes a
+ * flow::metaprogramming::container<T> as a template argument and handles it
+ *
+ * flow::for_each<int, double>([&]<typename message_t>(flow::metaprogramming::container<message_t> ) {
+ *   const auto& ch = std::any_cast<channel<message_t>>(channels.at(typeid(channel<message_t>)));
+ *   ch.do_work();
+ * });
+ *
+ * @tparam the_rest_t The items to be executed in this loop
+ * @param callback The function handling the type
+ */
+//template<typename... the_rest_t>
+//void for_each(auto&& callback)
+//{
+//  if constexpr (flow::metaprogramming::empty<the_rest_t...>()) {
+//    return;
+//  }
+//  else {
+//    using next_t = typename flow::metaprogramming::next<the_rest_t...>::type;
+//    callback(flow::metaprogramming::container<next_t>{});
+//    pop_and_continue<the_rest_t...>(std::forward<decltype(callback)>(callback));
+//  }
+//}
+
+template<typename completed_t, typename... message_ts>
+[[maybe_unused]] void pop_and_continue(auto&& callback)
+{
+  for_each<message_ts...>(std::forward<decltype(callback)>(callback));
 }
 
 /**
@@ -227,7 +267,7 @@ template <class... Types>
  * @return returns a view of the type
  */
 template<typename INTERFACE_TYPENAME>
-[[nodiscard]] consteval auto to_string() -> std::string_view
+[[maybe_unused]] [[nodiscard]] constexpr auto to_string() -> std::string_view
 {
   constexpr std::string_view result = __PRETTY_FUNCTION__;
   constexpr std::string_view templateStr = "INTERFACE_TYPENAME = ";
@@ -248,18 +288,6 @@ template<typename INTERFACE_TYPENAME>
   }
 }
 
-/**
- * Hash a type
- * @tparam INTERFACE_TYPENAME
- * @return
- */
-template<typename INTERFACE_TYPENAME>
-[[nodiscard]] consteval auto consteval_hash() -> uint64_t
-{
-  std::string_view name = to_string<INTERFACE_TYPENAME>();
-  return consteval_wyhash(&name[0], name.size(), 0);
-}
-
 template <typename T>
 struct function_traits
   : public function_traits<decltype(&T::operator())>
@@ -273,7 +301,7 @@ struct function_traits<ReturnType(ClassType::*)(Args...) const>
   enum { arity = sizeof...(Args) };
   // arity is the number of arguments.
 
-  typedef ReturnType result_type;
+  [[maybe_unused]] typedef ReturnType result_type;
 
   template <size_t i>
   struct argument
@@ -284,5 +312,6 @@ struct function_traits<ReturnType(ClassType::*)(Args...) const>
     // composed of those arguments.
   };
 };
+
 }
 #endif//FLOW_METAPROGRAMMING_HPP
--- test/tests/test_metaprogramming.cpp
+++ test/tests/test_metaprogramming.cpp
@@ -1,35 +1,157 @@
 #include <catch2/catch.hpp>
+#include <flow/metaprogramming.hpp>
 
-TEST_CASE( "vectors can be sized and resized", "[vector]" ) {
+namespace {
+struct Foo {
+};
 
-std::vector<int> v( 5 );
+template<typename t>
+struct Bar {
+};
 
-REQUIRE( v.size() == 5 );
-REQUIRE( v.capacity() >= 5 );
+template<typename... ts>
+struct Baz {
+};
+}// namespace
 
-SECTION( "resizing bigger changes size and capacity" ) {
-v.resize( 10 );
-
-REQUIRE( v.size() == 10 );
-REQUIRE( v.capacity() >= 10 );
-}
-SECTION( "resizing smaller changes size but not capacity" ) {
-v.resize( 0 );
-
-REQUIRE( v.size() == 0 );
-REQUIRE( v.capacity() >= 5 );
-}
-SECTION( "reserving bigger changes capacity but not size" ) {
-v.reserve( 10 );
-
-REQUIRE( v.size() == 5 );
-REQUIRE( v.capacity() >= 10 );
-}
-SECTION( "reserving smaller does not change size or capacity" ) {
-v.reserve( 0 );
-
-REQUIRE( v.size() == 5 );
-REQUIRE( v.capacity() >= 5 );
-}
+TEST_CASE("Test size_tc compile time size type", "[size_tc]")
+{
+  using namespace flow::metaprogramming;
+  constexpr auto a = size_tc<4>{};
+  constexpr auto b = size_tc(a);
+  STATIC_REQUIRE(b.data == 4);
 }
 
+TEST_CASE("Test the type container metaprogramming helper class", "[type_container]")
+{
+  using namespace flow::metaprogramming;
+  STATIC_REQUIRE(std::is_same_v<type_container<Foo>, type_container<Foo>>);
+  STATIC_REQUIRE(std::is_same_v<type_container<Foo>::type, type_container<Foo>::type>);
+  STATIC_REQUIRE_FALSE(std::is_same_v<type_container<Foo>, type_container<Bar<Foo>>>);
+  STATIC_REQUIRE_FALSE(std::is_same_v<type_container<Foo>::type, type_container<Bar<Foo>>::type>);
+
+  SECTION("copy and move constructors")
+  {
+    constexpr auto foo = type_container<Foo>{};
+    using container_foo_t = decltype(foo);
+    using foo_t = decltype(foo)::type;
+
+    [[maybe_unused]] constexpr auto copy = foo;// test copy constructor
+    using container_copy_t = decltype(copy);
+    using copy_t = decltype(copy)::type;
+
+    STATIC_REQUIRE(std::is_same_v<container_foo_t, container_copy_t>);
+    STATIC_REQUIRE(std::is_same_v<foo_t, copy_t>);
+
+    [[maybe_unused]] constexpr auto moved = std::move(foo);// test move constructor
+    using container_moved_t = decltype(moved);
+    using moved_t = decltype(moved)::type;
+
+    STATIC_REQUIRE(std::is_same_v<container_foo_t, container_moved_t>);
+    STATIC_REQUIRE(std::is_same_v<foo_t, moved_t>);
+  }
+
+  SECTION("conversion of tuple to type_container")
+  {
+    [[maybe_unused]] constexpr type_container<Foo> foo = std::tuple<Foo>{};
+  }
+}
+
+TEST_CASE("Test the size function", "[size]")
+{
+  using namespace flow::metaprogramming;
+  SECTION("Test one")
+  {
+    STATIC_REQUIRE(size<Foo>() == 1);
+    STATIC_REQUIRE(size<Bar<Foo>>() == 1);
+    STATIC_REQUIRE(size<Baz<Foo, Bar<Foo>>>() == 1);
+
+    STATIC_REQUIRE(size(std::tuple<Foo>{}) == 1);
+    STATIC_REQUIRE(size(std::tuple<Bar<Foo>>{}) == 1);
+    STATIC_REQUIRE(size(std::tuple<Baz<Foo, Bar<Foo>>>{}) == 1);
+  }
+
+  SECTION("Test two")
+  {
+    STATIC_REQUIRE(size<Foo, Bar<Foo>>() == 2);
+    STATIC_REQUIRE(size<Bar<Foo>, Foo>() == 2);
+    STATIC_REQUIRE(size<Baz<Foo, Bar<Foo>>, Foo>() == 2);
+
+    STATIC_REQUIRE(size(std::tuple<Foo, Bar<Foo>>{}) == 2);
+    STATIC_REQUIRE(size(std::tuple<Foo, Bar<Foo>>{}) == 2);
+    STATIC_REQUIRE(size(std::tuple<Bar<Foo>, Baz<Foo, Bar<Foo>>>{}) == 2);
+  }
+}
+
+TEST_CASE("Determine whether type containers are empty (e.g. contain no types)", "[empty]")
+{
+  using namespace flow::metaprogramming;
+  SECTION("Test multiple are not empty")
+  {
+    STATIC_REQUIRE_FALSE(empty<Foo>());
+    STATIC_REQUIRE_FALSE(empty<Foo, Foo>());
+    STATIC_REQUIRE_FALSE(empty<Foo, Bar<Foo>, Baz<Bar<Foo>, Foo, Foo>>());
+
+    STATIC_REQUIRE_FALSE(empty(std::tuple<Foo>{}));
+    STATIC_REQUIRE_FALSE(empty(std::tuple<Foo, Foo>{}));
+    STATIC_REQUIRE_FALSE(empty(std::tuple<Foo, Bar<Foo>, Baz<Bar<Foo>, Foo, Foo>>{}));
+  }
+
+  SECTION("Test empty cases")
+  {
+    STATIC_REQUIRE(empty<>());
+    STATIC_REQUIRE(empty());
+    STATIC_REQUIRE(empty(std::tuple<>{}));
+  }
+}
+
+TEST_CASE("Testing popping the first or next item from the list", "[pop_front]")
+{
+  using namespace flow::metaprogramming;
+  // popping empty doesn't compiler (which is what we want)
+  SECTION("Pop single items")
+  { // they should be empty...
+    STATIC_REQUIRE(empty(pop_front<Foo>()));
+    STATIC_REQUIRE(empty(pop_front<int>()));
+    STATIC_REQUIRE(empty(pop_front<Bar<Foo>>()));
+  }
+
+  SECTION("Pop size of two")
+  {
+    {
+      constexpr type_container<Foo> foo_t = pop_front<Foo, Foo>();// converts std::tuple
+      STATIC_REQUIRE(std::is_same_v<decltype(foo_t)::type, Foo>);
+    }
+    {// should pop Bar<int> out
+      constexpr type_container<Foo> foo_t = pop_front<Bar<int>, Foo>();
+      STATIC_REQUIRE(std::is_same_v<decltype(foo_t)::type, Foo>);
+      constexpr type_container<int> int_t = pop_front<Bar<int>, int>();
+      STATIC_REQUIRE(std::is_same_v<decltype(int_t)::type, int>);
+    }
+  }
+
+  SECTION("Pop two from two")
+  {
+      constexpr std::tuple<> empty_t = pop_front(pop_front<Foo, Foo>());
+      STATIC_REQUIRE(empty(empty_t));
+  }
+
+  SECTION("Pop two from three")
+  {
+    {
+      constexpr type_container<Bar<Foo>> bar_foo_t = pop_front<Foo, Foo, Bar<Foo>>(size_tc<2>{});
+      STATIC_REQUIRE(std::is_same_v<decltype(bar_foo_t)::type, Bar<Foo>>);
+    }
+
+    {
+      constexpr type_container<Bar<Foo>> bar_foo_t = pop_front(pop_front<Foo, Foo, Bar<Foo>>());
+      STATIC_REQUIRE(std::is_same_v<decltype(bar_foo_t)::type, Bar<Foo>>);
+    }
+  }
+
+  SECTION("Pop three from three")
+  {
+//    constexpr std::tuple<> empty_t = pop_front(pop_front<Foo, Foo, Bar<Foo>>());
+//    STATIC_REQUIRE(empty(empty_t));
+  }
+}
