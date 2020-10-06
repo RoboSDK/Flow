3417c44ee58e1f24ebedd835c3876e1e43cf33b7
Manuel Meraz <meraz.manuel@gmail.com>
2020-10-06 14:39:32 -0400

    create channel_set data structure to store multiple channel names with same type

---
+++ include/flow/data_structures/channel_set.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <any>
+
+#include "flow/channel.hpp"
+
+namespace flow {
+class channel_set {
+public:
+  template<typename message_t>
+  bool contains(std::string const& channel_name) const
+  {
+    return m_items.find(hash<message_t>(channel_name)) != m_items.end();
+  }
+
+  void put(auto&& channel)
+  {
+    m_items[channel.hash()] = std::forward<decltype(channel)>(channel);
+  }
+
+  template<typename message_t>
+  auto& at(std::string const& channel_name)
+  {
+    return std::any_cast<channel<message_t>&>(m_items.at(hash<message_t>(channel_name)));
+  }
+
+private:
+  template<typename message_t>
+  std::size_t hash(std::string const& channel_name) const
+  {
+    return typeid(message_t).hash_code() ^ std::hash<std::string>{}(channel_name);
+  }
+
+  std::unordered_map<std::size_t, std::any> m_items{};
+};
+}// namespace flow
