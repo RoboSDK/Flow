420762c9ae6723166ab5a935d11409cb3d246213
Manuel Meraz <meraz.manuel@gmail.com>
2020-10-13 19:35:44 -0400

    add constructors to mixed array

--- include/flow/data_structures/mixed_array.hpp
+++ include/flow/data_structures/mixed_array.hpp
@@ -1,12 +1,13 @@
 #ifndef FLOW_MIXED_ARRAY_HPP
 #define FLOW_MIXED_ARRAY_HPP
 
-#include <tuple>
+#include <algorithm>
 #include <array>
+#include <tuple>
 #include <variant>
 
-#include "flow/metaprogramming.hpp"
 #include "flow/logging.hpp"
+#include "flow/metaprogramming.hpp"
 
 namespace flow {
 
@@ -17,20 +18,45 @@ class mixed_array {
 
 public:
   template<typename... Types>
-  constexpr explicit mixed_array(Types &&... ts) : m_items{ { std::forward<Types>(ts)... } } {}
+  constexpr explicit mixed_array(Types&&... ts) : m_items{ { std::forward<Types>(ts)... } } {}
 
   using iterator = typename array_t::iterator;
   using const_iterator = typename array_t::const_iterator;
+  constexpr mixed_array() = default;
+  constexpr mixed_array(mixed_array const& other)
+  {
+    std::copy(std::begin(other), std::end(other), std::begin(*this));
+  }
 
+  constexpr mixed_array (mixed_array && other) noexcept
+  {
+    for (std::size_t i = 0; i < N; ++i) {
+      m_items[i] = std::move(other.m_items[i]);
+    }
+  }
+
+  constexpr mixed_array& operator=(mixed_array const& other)
+  {
+    std::copy(std::begin(other), std::end(other), std::begin(*this));
+    return *this;
+  }
+
+  constexpr mixed_array& operator=(mixed_array && other) noexcept
+  {
+    for (std::size_t i = 0; i < N; ++i) {
+      m_items[i] = std::move(other.m_items[i]);
+    }
+    return *this;
+  }
   constexpr decltype(auto) begin() { return std::begin(m_items); }
   constexpr decltype(auto) begin() const { return std::begin(m_items); }
   constexpr decltype(auto) end() { return std::end(m_items); }
   constexpr decltype(auto) end() const { return std::end(m_items); }
 
-  [[maybe_unused]] constexpr mixed_t&front() const { return m_items.front(); }
-  [[maybe_unused]] constexpr mixed_t&back() const { return m_items.back(); }
+  [[maybe_unused]] constexpr mixed_t& front() const { return m_items.front(); }
+  [[maybe_unused]] constexpr mixed_t& back() const { return m_items.back(); }
 
-  constexpr mixed_t&operator[](std::size_t index) { return m_items[index]; }
+  constexpr mixed_t& operator[](std::size_t index) { return m_items[index]; }
   [[maybe_unused]] constexpr std::size_t size() const { return m_items.size(); }
 
 private:
@@ -42,7 +68,8 @@ constexpr auto make_mixed_array(types_t&&... types)
 {
   auto type_set = metaprogramming::make_type_set<types_t...>();
 
-  const auto to_mixed_array = [&]<typename... TypeSet>(std::tuple<TypeSet...> /*unused*/) {
+  const auto to_mixed_array = [&]<typename... TypeSet>(std::tuple<TypeSet...> /*unused*/)
+  {
     return mixed_array<sizeof...(types), TypeSet...>(types...);
   };
 
@@ -50,15 +77,17 @@ constexpr auto make_mixed_array(types_t&&... types)
 }
 
 template<class... Ts>
-struct overloaded : Ts... { using Ts::operator()...; };
+struct overloaded : Ts... {
+  using Ts::operator()...;
+};
 template<class... Ts>
 overloaded(Ts...) -> overloaded<Ts...>;
 
 template<typename... Visitors>
-constexpr auto make_visitor(Visitors &&... visitors)
+constexpr auto make_visitor(Visitors&&... visitors)
 {
-  return [&](auto &item) {
-    return std::visit(overloaded{ std::forward<decltype(visitors)>(visitors) ... }, item);
+  return [&](auto& item) {
+    return std::visit(overloaded{ std::forward<decltype(visitors)>(visitors)... }, item);
   };
 }
 
--- test/unit_tests/test_mixed_array.cpp
+++ test/unit_tests/test_mixed_array.cpp
@@ -3,16 +3,50 @@
 #include <iostream>
 #include <sstream>
 
-TEST_CASE("vectors can be sized and resized", "[vector]") {
-  constexpr auto arr = flow::make_mixed_array(1, 2, 3.2, "dog", 100UL);
-
+void verify(auto& arr) {
   REQUIRE(arr.size() == 5);
 
   std::stringstream ss;
   std::for_each(std::begin(arr), std::end(arr), flow::make_visitor([&ss](auto& item) {
-    ss << item << " ";
+         ss << item << " ";
   }));
 
   using namespace std::string_literals;
   REQUIRE(ss.str() == "1 2 3.2 dog 100 "s);
+}
+
+TEST_CASE("Mixed array has values it is constructed with", "[mixed_array]")
+{
+  constexpr auto arr = flow::make_mixed_array(1, 2, 3.2, "dog", 100UL);
+  verify(arr);
+}
+
+TEST_CASE("Test constructors and assignment operations", "[mixed_array]")
+{
+  SECTION("Test copy operator=")
+  {
+    constexpr auto arr = flow::make_mixed_array(1, 2, 3.2, "dog", 100UL);
+    constexpr auto copy = arr;
+    verify(copy);
+  }
+
+  SECTION("Test copy constructor")
+  {
+    constexpr auto arr = flow::make_mixed_array(1, 2, 3.2, "dog", 100UL);
+    constexpr decltype(arr) copy{arr};
+    verify(copy);
+  }
+
+  SECTION("Test move operator=")
+  {
+    constexpr auto arr = flow::make_mixed_array(1, 2, 3.2, "dog", 100UL);
+    constexpr auto moved = std::move(arr);
+    verify(moved);
+  }
+
+  SECTION("Test move constructor")
+  {
+    constexpr auto moved(flow::make_mixed_array(1, 2, 3.2, "dog", 100UL));
+    verify(moved);
+  }
 }
