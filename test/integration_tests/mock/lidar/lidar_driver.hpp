6bea4084a6b4fe473cfccffdde730cc409a494c6
Manuel Meraz <meraz.manuel@gmail.com>
2020-10-14 19:19:51 -0400

    pre computer lidar messages in driver

--- test/integration_tests/mock/lidar/lidar_driver.hpp
+++ test/integration_tests/mock/lidar/lidar_driver.hpp
@@ -6,13 +6,28 @@
 
 namespace mock {
 struct lidar_driver {
-  lidar_message drive()
+  static constexpr std::size_t NUM_PRECOMPUTED_MESSAGES = 1'000;
+  lidar_driver() {
+    std::generate(std::begin(messages), std::end(messages), [this]{ return make_message(); });
+  }
+
+  lidar_message make_message()
   {
     lidar_message data{};
     std::generate_n(std::back_inserter(data.points), lidar_message::capacity, [this] { return dist(rd); });
     return data;
   }
+
+  lidar_message drive()
+  {
+    return messages[current_message % NUM_PRECOMPUTED_MESSAGES];
+  }
+
+
   std::random_device rd;
   std::uniform_real_distribution<double> dist{ 0, 10 };
+
+  std::size_t current_message{};
+  std::array<lidar_message, NUM_PRECOMPUTED_MESSAGES> messages{};
 };
 }// namespace mock
