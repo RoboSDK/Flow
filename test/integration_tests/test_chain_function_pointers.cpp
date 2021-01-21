4331724740db6e91fc78871907700316e2f29862
Manuel Meraz <meraz.manuel@gmail.com>
2021-01-20 20:26:03 -0500

    add in atomic support

---
+++ test/integration_tests/test_chain_function_pointers.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+
+#include <flow/configuration.hpp>
+#include <flow/context.hpp>
+#include <flow/logging.hpp>
+#include <flow/network.hpp>
+
+#include <cppcoro/sync_wait.hpp>
+
+using namespace std::literals;
+
+int producer()
+{
+  flow::logging::error("producer");
+  static int val = 0;
+  return val++;
+}
+
+int doubler(int&& val)
+{
+  flow::logging::error("transformer");
+  return val * 2;
+}
+
+void consumer(int&& val)
+{
+  flow::logging::error("consumer: {}", val);
+}
+
+void consumer2(int&& val)
+{
+  flow::logging::error("consumer2: {}", val);
+}
+
+int main()
+{
+  using context_t = flow::context<flow::configuration>;
+  using chain_t = flow::network<flow::configuration>;
+
+  auto context = std::make_unique<context_t>();
+  chain_t chain{ context.get() };
+
+  chain.push(producer, "producer");
+  chain.push(doubler, "producer", "doubler");
+  chain.push(consumer, "doubler");
+  chain.push(consumer2, "producer");
+
+//  chain.cancel_after(1ms);
+  cppcoro::sync_wait(chain.spin());
+}
---
+++ test/integration_tests/test_chain_lambdas.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+
+#include <flow/configuration.hpp>
+#include <flow/context.hpp>
+#include <flow/logging.hpp>
+#include <flow/network.hpp>
+
+#include <cppcoro/sync_wait.hpp>
+
+using namespace std::literals;
+
+int main()
+{
+  using context_t = flow::context<flow::configuration>;
+  using chain_t = flow::network<flow::configuration>;
+
+  auto context = std::make_unique<context_t>();
+  chain_t chain{ context.get() };
+
+  chain.push([] {
+    static int val = 0;
+    return val++;
+  },
+    "producer");
+
+  chain.push([](int&& val) {
+    return 2 * val;
+  },
+    "producer",
+    "doubler");
+
+  chain.push([](int&& /*unused*/) {}, "doubler");
+
+  chain.cancel_after(0ms);
+  cppcoro::sync_wait(chain.spin());
+}
