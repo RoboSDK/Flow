4ac58ef0e8d6e9168634282e91b8417613d766cb
Manuel Meraz <meraz.manuel@gmail.com>
2021-01-25 13:30:09 -0500

    change stack to queue when producing messages

--- include/flow/detail/producer_token.hpp
+++ include/flow/detail/producer_token.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <stack>
+#include <queue>
 #include <cppcoro/sequence_range.hpp>
 
 namespace flow::detail {
 
 template <typename message_t>
 struct producer_token {
-  std::stack<message_t> messages{};
+  std::queue<message_t> messages{};
   cppcoro::sequence_range<std::size_t> sequences{};
 };
 }
