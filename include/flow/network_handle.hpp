09c888723e0658c70dec65a5e310271c8e142608
Manuel Meraz <meraz.manuel@gmail.com>
2021-01-25 12:43:19 -0500

    add documentation to network handle

--- include/flow/network_handle.hpp
+++ include/flow/network_handle.hpp
@@ -2,6 +2,10 @@
 
 #include "flow/detail/cancellation_handle.hpp"
 
+/**
+ * The network handle is used to request cancellation of the network by the owner of the handle
+ */
+
 namespace flow {
 class network_handle {
 public:
@@ -9,6 +13,9 @@ public:
     m_handles.push_back(handle);
   }
 
+  /**
+   * Requests cancellation of the network routines and will begin cancellation
+   */
   void request_cancellation()
   {
     for (auto& handle : m_handles) handle.request_cancellation();
