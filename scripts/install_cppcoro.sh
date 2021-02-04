7f305898a2ce08ba8da928ef4b3352c2f638f4dc
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-04 18:48:33 -0500

    build cppcoro in release mode instead of debug

--- scripts/install_cppcoro.sh
+++ scripts/install_cppcoro.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Garcia6l20/cppcoro.git
 cd cppcoro || exit 1
 mkdir build
 cd build || exit 1
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 
 cp ./lib/libcppcoro.a /usr/local/lib
