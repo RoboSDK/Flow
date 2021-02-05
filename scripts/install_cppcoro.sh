1036f21b343f93e7855b5c0fa766d106e03021ab
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-04 19:39:41 -0500

    update cppcoro script

--- scripts/install_cppcoro.sh
+++ scripts/install_cppcoro.sh
@@ -2,13 +2,6 @@
 
 cd /tmp || exit 1
 git clone https://github.com/Garcia6l20/cppcoro.git
-cd cppcoro || exit 1
-mkdir build
-cd build || exit 1
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j4
-
-cp ./lib/libcppcoro.a /usr/local/lib
-cp -r ../include/* /usr/local/include
-
-cd ~ || exit 1
+mkdir cppcoro/build && cd cppcoro/build
+cmake -DCMAKE_BUILD_TYPE=Release -DCPPCORO_USE_IO_RING=ON ..
+make -j && sudo make install
