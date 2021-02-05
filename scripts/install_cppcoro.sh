5b615e4cbe030d911b5c5ad01799abeabc547fcc
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-04 19:40:21 -0500

    remove sudo

--- scripts/install_cppcoro.sh
+++ scripts/install_cppcoro.sh
@@ -4,4 +4,4 @@ cd /tmp || exit 1
 git clone https://github.com/Garcia6l20/cppcoro.git
 mkdir cppcoro/build && cd cppcoro/build
 cmake -DCMAKE_BUILD_TYPE=Release -DCPPCORO_USE_IO_RING=ON ..
-make -j && sudo make install
+make -j && make install
