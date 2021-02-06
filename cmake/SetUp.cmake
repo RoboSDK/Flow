943ba51b960b06ab66d5d75ba8d1b565f14291f0
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-06 15:50:28 -0500

    export cppcoro wel enough to find it when finding flow

--- cmake/SetUp.cmake
+++ cmake/SetUp.cmake
@@ -44,8 +44,5 @@ enable_doxygen()
 # allow for static analysis options
 include(cmake/StaticAnalyzers.cmake)
 
-configure_file(cmake/Findcppcoro.cmake lib/cmake/cppcoro/cppcoroConfig.cmake)
-install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib/cmake DESTINATION lib)
-
 configure_file(cmake/Findliburing.cmake lib/cmake/liburing/liburingConfig.cmake)
 install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib/cmake DESTINATION lib)
