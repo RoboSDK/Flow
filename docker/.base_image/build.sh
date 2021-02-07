e9861fb536d249511329e0fa42794095cb2dbc00
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-06 20:11:04 -0500

    cleanedup base image docker scripts to be able to work in the container

--- docker/.base_image/build.sh
+++ docker/.base_image/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-DOCKER_NAME='manuelmeraz/flow:base'
+set -o nounset
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+source "${DIR}/../docker-options"
+TAG="base"
 
 docker build \
-  $@ \
-  --network=host \
-  -t ${DOCKER_NAME} .
+    --build-arg DOCKER_UID="${DOCKER_UID}" \
+    --build-arg DOCKER_GID="${DOCKER_GID}" \
+    --build-arg DOCKER_USERNAME="${DOCKER_USERNAME}" \
+    -t ${VARIANT}:${TAG} .
