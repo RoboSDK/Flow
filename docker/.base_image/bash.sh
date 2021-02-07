ab4447b6058c5e579163bf7143e63529d5887e42
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-06 22:10:33 -0500

    add sudo to base image

--- docker/.base_image/bash.sh
+++ docker/.base_image/bash.sh
@@ -4,4 +4,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "${DIR}/../docker-options"
 
-docker exec -it ${DOCKER_NAME} bash 
+docker exec -u root -it ${DOCKER_NAME} bash 
