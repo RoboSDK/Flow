6d8fa6209a6366da40b3e590c7ff7a5bfde6fd50
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-16 20:17:37 -0500

    remove external

--- cmake/SetUp.cmake
+++ cmake/SetUp.cmake
@@ -2,7 +2,6 @@
 set(INSTALL_LIB_DIR lib CACHE PATH "Installation directory for libraries")
 set(INSTALL_BIN_DIR bin CACHE PATH "Installation directory for executables")
 set(INSTALL_INCLUDE_DIR include/flow CACHE PATH "Installation directory for header files")
-set(INSTALL_EXTERNAL_DIR external CACHE PATH "Installation directory for external header files")
 set(INSTALL_CMAKE_DIR lib/cmake/flow CACHE PATH "Installation directory for CMake files")
 
 # Make relative paths absolute (needed later on)
@@ -17,8 +16,6 @@ configure_file(cmake/config.h.in "${CMAKE_CURRENT_BINARY_DIR}/config.h" @ONLY)
 
 include(cmake/StandardProjectSettings.cmake)
 include(cmake/PreventInSourceBuilds.cmake)
-
-add_subdirectory(external)
 
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 target_compile_features(project_options INTERFACE cxx_std_20)
